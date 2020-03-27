1162c9165d4eff590ceb32c2c5e63e7d0c5d7b2f
Dobre <dobremi91@gmail.com>
2020-03-27 19:32:23 +0200

    hided the api key

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from "contentful";
 
 export default createClient({
-  space: "3ar9bcm6xook",
-  accessToken: "t6BKbMYYc_Ossb9kE5kEg-X00o_6-3DMy2plUZ_NATA"
+  space: process.env.REACT_APP_API_SPACE,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });
